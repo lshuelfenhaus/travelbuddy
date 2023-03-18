fd32e71fad8fc8399095e3a8b62d2d1298e7e1a4
anhquang2605 <anhquang2605@gmail.com>
2023-03-17 18:04:49 -0700

    removed the need for trailing in App Bar, rearranged the button orders

--- components/bottomnavigation.tsx
+++ components/bottomnavigation.tsx
@@ -19,45 +19,44 @@ export const BottomNavigation = (props: BottomNavigationProps) => {
     } 
     return (
         <>
-            <Modal animationIn="slideInRight" animationOut="slideOutRight" isVisible={modalVisible}>
+{/*             <Modal animationIn="slideInRight" animationOut="slideOutRight" isVisible={modalVisible}>
                 <VStack style={styles.menuContainer}>
                     <IconButton style={styles.escape} icon={<AntDesign onPress={event => setModalVisible(false)} name="arrowright" size={50} color="white" /> } />
                     <Button titleStyle={{color: "white"}}style={styles.menuButtonLogout} variant="outlined" title="Logout" onPress={logOut}/>
                     <Button style={styles.menuButton} variant="outlined" title="Setting" onPress={null}/>
                     <Button style={styles.menuButton} variant="outlined" title="Another menu option" onPress={null}/>
                 </VStack>
-            </Modal>
+            </Modal> */}
             <AppBar
                 color="primary"
-                variant="bottom"      
+                variant="bottom"  
+                contentContainerStyle={styles.contentContainer}   
+                leadingContainerStyle={styles.leadingContainer}
                 leading={(
-                <HStack spacing={SPACING}>
+                <HStack style={styles.hstack} spacing={SPACING}>
+                                        
                     <View style={styles.iconContainer}>
-                        <IconButton style={styles.icon} icon={props => <Entypo name="list" size={ICONSIZE} color="white" />  } />
-                        <Text style={styles.textIcon}>Itinerary</Text>
-                    </View>
-                    <View style={styles.iconContainer}>
-                        <IconButton style={styles.icon} icon={<Ionicons color="white" size={ICONSIZE} name="chatbubble-ellipses" /> } />
-                        <Text style={styles.textIcon}>Buddy</Text> 
+                        <IconButton icon={<MaterialCommunityIcons name="home-heart" size={ICONSIZE} color="white"/>} />
+                        <Text style={styles.textIcon}>Home</Text> 
                     </View>    
             
+                    <View style={styles.iconContainer}>
+                        <IconButton icon={<Ionicons color="white" size={ICONSIZE} name="chatbubble-ellipses" /> } />
+                        <Text style={styles.textIcon}>Buddy</Text> 
+                    </View>    
+                    <IconButton style = {styles.centralIcon} icon={<Ionicons style={ {textAlign:"center"}} name="add" size={50} color="#6200EE" /> }/>
+                    <View style={styles.iconContainer}>
+                            <IconButton  icon={props => <Entypo name="list" size={ICONSIZE} color="white" />  } />
+                            <Text style={styles.textIcon}>Itinerary</Text>
+                    </View>
+                    <View style={styles.iconContainer}>
+                        <IconButton  icon={<MaterialCommunityIcons name="account" size={ICONSIZE} color="white" />}/>
+                        <Text style={styles.textIcon}>Account</Text> 
+                    </View>
                 </HStack>
-                )}
-                trailing={props => (
-                    <HStack spacing={SPACING}>
-                        <View style={styles.iconContainer}>
-                            <IconButton style={styles.icon} icon={<MaterialCommunityIcons name="account" size={ICONSIZE} color="white" />}/>
-                            <Text style={styles.textIcon}>Account</Text> 
-                        </View>
-                        <View style={styles.iconContainer}>
-                            <IconButton style={styles.icon} icon={props => <Entypo onPress={event => setModalVisible(true)} name="dots-three-horizontal" size={ICONSIZE} color="white" />  } />
-                            <Text style={styles.textIcon}>More</Text> 
-                        </View>
-                    </HStack>
                 )}
                 style={styles.navigationBar}
             >
-            <IconButton style = {styles.centralIcon} icon={<Ionicons style={ {textAlign:"center"}} name="add" size={50} color="#6200EE" /> }/>
             </AppBar>
         </>
     )
@@ -75,6 +74,15 @@ const styles = StyleSheet.create({
         width: 300,
         marginTop: 8
     },
+    hstack:{ 
+        width: "100%"
+    },
+    contentContainer:{alignItems: "center", justifyContent: "center", flexDirection:"column"},
+    leadingContainer:{
+       marginStart: 0,
+       marginEnd: 0,
+       marginBottom: 8
+    },
     menuButtonLogout:{
         backgroundColor: "tomato",
         paddingVertical: 4,
@@ -85,28 +93,24 @@ const styles = StyleSheet.create({
     iconContainer: {
         alignItems: "center"
     },
-    icon: {
-        position: 'absolute',
-        top: -10,
-    },
+
     centralIcon:{
-        position: 'absolute',
         borderRadius: 30,
         backgroundColor: 'white',
         justifyContent: "center",
         alignItem: "center",
         textAlign: "center",
-        top: -8,
         width: 60,
         height: 60,
         borderWidth: 1,
+        marginTop: -10,
         borderColor: OURPURPLE
     },
     textIcon: {
         color: "white",
         fontSize: 12,
         textAlign: 'center',
-        marginTop: 30
+        marginTop: -8
     },
     navigationBar:{
         position: "absolute", 
@@ -114,9 +118,6 @@ const styles = StyleSheet.create({
         end: 0, 
         bottom: 0, 
         height: 55,
-        paddingHorizontal: 16,
-        alignItems: "center",
-        justifyContent: "center"
     },
     menuContainer:{
         height:"100%",
@@ -124,6 +125,5 @@ const styles = StyleSheet.create({
         flexDirection: "column-reverse",
         alignItems: "center",
         borderRadius: 10,
-        padding: 12
     }
 })
