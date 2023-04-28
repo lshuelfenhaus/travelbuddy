b8cc508621ac51aa59f016ef1e6f830cca8a70eb
anhquang2605 <anhquang2605@gmail.com>
2023-04-28 16:16:06 -0700

    added useEffect

--- screens/ChatbotScreen.tsx
+++ screens/ChatbotScreen.tsx
@@ -27,10 +27,9 @@ export function Chatbot(props: HomeScreenProps) {
     apiKey: process.env.OPENAI_API_KEY,
   });
   const openai = new OpenAIApi(configuration);
+  const [sent, setSent] = useState<boolean>(false);
   const [messages, setMessages] = useState<Array<Message>>([]);
   const [aiMessages, setAiMessages] = useState<Array<ChatCompletionRequestMessage>>([]);
-  console.log(aiMessages)
-  console.log(messages)
   useEffect(() => {
     setMessages([
       {
@@ -44,49 +43,60 @@ export function Chatbot(props: HomeScreenProps) {
         },
       },
     ])
-  }, [])
-
-  const onSend = useCallback(async (messages:Array<Message> = []) => {
-    console.log(configuration.apiKey);
-    setMessages(previousMessages => GiftedChat.append(previousMessages, messages));
-    // setAiMessages(previousMessages => [...previousMessages, {'role': 'user', 'content': messages[0].text}]);
+  }, []);
+  const sendToAPI = async () => {
+    setSent(false);
     try{
-    const apiRequestBody = {
-      model: "gpt-3.5-turbo",
-      messages: aiMessages
+        const apiRequestBody = {
+          model: "gpt-3.5-turbo",
+          messages: aiMessages
+        }
+        const completions = await openai.createChatCompletion(apiRequestBody)
+        if(!completions.data.choices[0].message?.content){
+          return
+        }
+        const botMessage: Message = {
+          _id: ++chatCounter,
+          text: completions.data.choices[0].message?.content,
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'TravelBuddy AI',
+            avatar: 'https://cdn.dribbble.com/users/722835/screenshots/4082720/bot_icon.gif',
+          },
+        };
+        setMessages(previousMessages => GiftedChat.append(previousMessages, [botMessage]))
+        setAiMessages(previousMessages => [...previousMessages, {'role': 'assistant', 'content': messages[0].text}])
+      //setMessages(previousMessages => GiftedChat.append(previousMessages, [botMessage]))
+     //setAiMessages(previousMessages => [...previousMessages, {'role': 'assistant', 'content': messages[0].text}])
+      }
+      catch(error){
+        const botMessage : Message = {
+          _id: ++chatCounter,
+          text: 'An error was thrown',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'TravelBuddy AI',
+            avatar: 'https://cdn.dribbble.com/users/722835/screenshots/4082720/bot_icon.gif',
+          },
+        };
+        setMessages(previousMessages => GiftedChat.append(previousMessages, [botMessage]))
+        setAiMessages(previousMessages => [...previousMessages, {'role': 'assistant', 'content': messages[0].text}])
+      }
+    
+    return
+  
+  }
+  useEffect(() => {
+    if(messages.length > 0 && aiMessages.length > 0 && sent){
+      sendToAPI();
     }
-    const completions = await openai.createChatCompletion(apiRequestBody)
-    if(!completions.data.choices[0].message?.content){
-      return
-    }
-    const botMessage: Message = {
-      _id: ++chatCounter,
-      text: completions.data.choices[0].message?.content,
-      createdAt: new Date(),
-      user: {
-        _id: 2,
-        name: 'TravelBuddy AI',
-        avatar: 'https://cdn.dribbble.com/users/722835/screenshots/4082720/bot_icon.gif',
-      },
-    };
-    //setMessages(previousMessages => GiftedChat.append(previousMessages, [botMessage]))
-   //setAiMessages(previousMessages => [...previousMessages, {'role': 'assistant', 'content': messages[0].text}])
-    }
-    catch(error){
-      console.log(error)
-      const botMessage : Message = {
-        _id: ++chatCounter,
-        text: 'An error was thrown',
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'TravelBuddy AI',
-          avatar: 'https://cdn.dribbble.com/users/722835/screenshots/4082720/bot_icon.gif',
-        },
-      };
-      setMessages(previousMessages => GiftedChat.append(previousMessages, [botMessage]))
-      setAiMessages(previousMessages => [...previousMessages, {'role': 'assistant', 'content': messages[0].text}])
-    }
+  },[messages]);
+  const onSend = useCallback(async (messages:Array<Message> = []) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages));
+    setAiMessages(previousMessages => [...previousMessages, {'role': 'user', 'content': messages[0].text}]);
+    setSent(true);
 
   }, [])
 
